fa1d6fdffb50c41f16a1424dfc6b49fbb6675f48
MrColaCandy <MahmoodKhdadady10@gmail.com>
2023-11-02 01:58:40 +0330

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import formData from "./data/form.json";
+const App = () => {
+  const renderInputField = (inputData) => {
+    if (inputData.type === "select") {
+      return (
+        <div key={inputData.id}>
+          <label htmlFor={inputData.id}>{inputData.label}</label>
+          <select name={inputData.name} id={inputData.id}>
+            {inputData.options?.map((op) => {
+              return (
+                <option value={op.value} key={op.id}>
+                  {op.label}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      );
+    } else if (inputData.type === "radio") {
+      return (
+        <div key={inputData.id}>
+          {inputData.options.map((op, index) => {
+            return (
+              <div key={op.value}>
+                <label htmlFor={index}>{op.label}</label>
+                <input name={inputData.name} id={index} type="radio" />
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div key={inputData.id}>
+          <label htmlFor={inputData.id}>{inputData.label}</label>
+          <input
+            placeholder={inputData.placeholder}
+            name={inputData.name}
+            type={inputData.type}
+            required={inputData.required}
+          />
+        </div>
+      );
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form">
+      {formData.map((inputData) => {
+        return <div key={inputData.id}>{renderInputField(inputData)}</div>;
+      })}
+    </form>
   );
-}
+};
 
 export default App;
